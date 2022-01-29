1b115c96280f8c36c6d05214a94151f14a83cebf
Marcin <marcin.kurpiel@gmail.com>
2022-01-29 12:15:31 +0100

    First commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { createApp } from 'vue'
+import { createApp, Vue } from 'vue'
 import App from './App.vue'
+import MaterialKit from "./assets/js/plugins/material-kit.js"
 import router from './router'
 import store from './store'
+import './assets/tailwind.css'
+import Amplify from 'aws-amplify'
+import '@aws-amplify/ui-vue'
+import aws_exports from './aws-exports'
+
+Amplify.configure(aws_exports)
+
+Vue.use(MaterialKit)
 
 createApp(App).use(store).use(router).mount('#app')
